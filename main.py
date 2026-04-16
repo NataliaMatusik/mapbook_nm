7aef9b328841e4d0896fcae0f2f2abfe08879a1e
NataliaMatusik <natalia.matusik@student.wat.edu.pl>
2026-04-16 14:00:31 +0200

    wip: mwc template applied

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-users: list = [
-    {'username': 'oliwia', 'location': 'lodz', 'posts': 1,
-     'usermessage': ['zyczenia1', 'kocham legie1', 'sprzedam opla1', 'kiwi1']},
-    {'username': 'paweł', 'location': 'ostroda', 'posts': 2,
-     'usermessage': ['zyczenia2', 'kocham legie2', 'sprzedam opla2']},
-    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['zyczenia3', 'kocham legie3']},
-    {'username': 'filip', 'location': 'deblin', 'posts': 4,
-     'usermessage': ['zyczenia4', 'kocham legie4', 'sprzedam opla4', 'kiwi4']},
-]
+from mapbook_lib.model import users
+from mapbook_lib.controller import read_data
 
-
-def read_data(users_data: list) -> None:
-    for user in users_data:
-        print(
-            f'twój znajomy {user['username']} z miejscowosci {user['location']}) opublikowal {user['posts']} wiadomosci. Ostatnia wiadomosc {user['usermessage'][-1]}')
 
 
 
