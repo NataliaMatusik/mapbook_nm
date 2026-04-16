537147c802e0b5b53325ba8fa432e67a40541b3b
NataliaMatusik <natalia.matusik@student.wat.edu.pl>
2026-04-16 13:19:30 +0200

    wip: completex data structure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-zmienna_1: str = 'oliwia'
-zmienna_2: str = 'paweł'
-zmienna_3: str = 'eliza'
-zmienna_4: str = 'filip'
-zmienna_5: str = 'ewelina'
-zmienna_6: str = 'weronika'
 
-users:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4,zmienna_5,zmienna_6]
 
-for user in users:
-    print(user)
+users:list=[
+    {'username':'oliwia', 'location':'lodz','posts': 1,'usermessage':['zyczenia1','kocham legie1','sprzedam opla1','kiwi1']},
+    {'username':'paweł', 'location':'ostroda','posts':2,'usermessage':['zyczenia2','kocham legie2','sprzedam opla2']},
+    {'username':'eliza', 'location':'radom','posts': 3,'usermessage':['zyczenia3','kocham legie3']},
+    {'username':'filip', 'location':'deblin','posts': 4,'usermessage':['zyczenia4','kocham legie4','sprzedam opla4','kiwi4']},
+]
+
+for user in users [1:]:
+    print(f'twój znajomy {user['username']} z miejscowosci {user['location']}) opublikowal {user['posts']} wiadomosci. Ostatnia wiadomosc {user['usermessage'][-1]}')
+
+
+#     twoj znajomy filip z miejscowosci deblin opublikowal 1 post o tresci: zyczenia
